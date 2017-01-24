f5429c8307056db45a492c9479ec241c44177ee9
Alex Mackenzie <AMackenzie@agily.local>
2017-01-24 16:24:55 +0100

    CR XC - expand / collapse code review

---
+++ src/views/widgets/form-user-full-body.ts
@@ -0,0 +1,17 @@
+import { bindable, bindingMode } from 'aurelia-framework';
+import * as Constants from '../../resources/constants';
+const CV = Constants
+
+export class FormUserFullBody {
+    @bindable custBody = null;
+    @bindable custClass = null;
+    @bindable custTitle = null;
+    @bindable custXc = null;
+    @bindable custXcId = null;
+    @bindable custXcExpanded = null;
+    @bindable custXcResClass = null;
+
+    public constructor(model) {
+
+    }
+}
