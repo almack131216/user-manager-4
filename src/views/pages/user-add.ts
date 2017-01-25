382be81eb2f5d70ecd30a5ee68ea6e06f5dafcf6
Alex Mackenzie <AMackenzie@agily.local>
2017-01-25 16:47:25 +0100

    NPM add aurelia-fetch-client

--- src/views/pages/user-add.ts
+++ src/views/pages/user-add.ts
@@ -5,10 +5,19 @@ import { UserUpdated, UserViewed } from '../../resources/messages';
 import { areEqual } from '../../api/utility';
 import {bindable,autoinject} from 'aurelia-framework';
 
+interface User {
+  first_name: string;
+  last_name: string;
+  email: string;
+  cell_number: string;
+}
 
 @inject(Element)
-export class UserAdd {  
-  @bindable user;
+export class UserAdd {
+  @bindable user = null;
+  routeConfig;
+  originalUser;
+
   title = 'Add User'
   newUser = [];
 
@@ -17,6 +26,21 @@ export class UserAdd {
     this.newUser.push({name:'last_name', val:'user.last_name', label: 'Las Nameee'});
     this.newUser.push({name:'email', val:'user.email', label: 'Emaileee'});
     this.newUser.push({name:'cell_number', val:'user.cell_number', label: 'Phoneee'});
+  }
+
+  get canSave() {
+    return this.user.first_name && this.user.last_name && !this.api.isRequesting;
+  }
+
+  save() {
+    this.api.saveUser(this.user).then(user => {
+      console.log('save this.user: ' + JSON.stringify(this.originalUser));
+      console.log('save user: ' + JSON.stringify(user));
+      this.user = <User>user;
+      //this.routeConfig.navModel.setTitle(this.user.first_name);
+      this.originalUser = JSON.parse(JSON.stringify(this.user));
+      this.ea.publish(new UserUpdated(this.user));
+    });
   }
 
 }
--- src/views/widgets/user-panels/user-panel-details.ts
+++ src/views/widgets/user-panels/user-panel-details.ts
@@ -1,5 +1,37 @@
-import { bindable } from 'aurelia-framework';
+import { HttpClient, json } from 'aurelia-fetch-client';
+import { autoinject, bindable } from 'aurelia-framework';
 
+
+@autoinject
 export class UserPanelDetails {
     @bindable user;
+    http:HttpClient
+
+    constructor(http:HttpClient) {
+        this.http = http;
+        this.getSomeJson();
+        // this.http.fetch('src/views/widgets/user-panels/dummy-data.json')
+        //     .then(response => response.json())
+        //     .then(data => {
+        //             console.log('getSomeJson: ' + data);
+        //     })
+    }
+
+    getSomeJson() {
+        this.http.fetch('src/views/widgets/user-panels/dummy-data.json')
+            .then(response => response.json())
+            .then(data => {
+                    console.log('getSomeJson: ' + JSON.stringify(data));
+            })
+    }
+
+    activate(){    
+        console.log('getSomeJson: ');
+
+        this.http.fetch('./dummy-data.json')
+            .then(response => response.json())
+            .then(data => {
+                    console.log('getSomeJson: ' + data);
+            })
+    }
 }
