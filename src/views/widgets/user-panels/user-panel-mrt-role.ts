025140d171a6fb2bfaa8da578a7d69bcfc671bf0
Alex Mackenzie <AMackenzie@agily.local>
2017-01-31 16:01:53 +0100

    MRT-role html done

--- src/views/widgets/user-panels/user-panel-mrt-role.ts
+++ src/views/widgets/user-panels/user-panel-mrt-role.ts
@@ -1,3 +1,22 @@
+import { bindable } from 'aurelia-framework';
+
 export class UserPanelMrtRole {
-    
+    @bindable user;
+    public sel_mrt_1_ics;
+    public sel_mrt_2_ics;
+
+    constructor() {
+        this.sel_mrt_1_ics = [
+            { "value": 1, "label": "Primary 1" },
+            { "value": 2, "label": "Primary 2" },
+            { "value": 3, "label": "Primary 3" }
+        ];
+
+        this.sel_mrt_2_ics = [
+            { "value": 1, "label": "Secondary 1" },
+            { "value": 2, "label": "Secondary 2" },
+            { "value": 3, "label": "Secondary 3" }
+        ];
+    }
+
 }
