6014405bbbf444cd549b478b53188ce2e15bf053
Alex Mackenzie <AMackenzie@agily.local>
2017-01-31 16:12:26 +0100

    CR: move sel_ arrays from .json into class

--- src/views/widgets/user-panels/user-panel-details.ts
+++ src/views/widgets/user-panels/user-panel-details.ts
@@ -3,6 +3,62 @@ import * as Constants from '../../../resources/constants';
 const CV = Constants
 
 export class UserPanelDetails {
+    @bindable user;
     public CV = CV;
-    @bindable user;    
+
+    public sel_region;
+    public sel_hub;
+    public sel_segment;
+    public sel_entity;
+    public sel_bp_office_address;
+
+    constructor() {
+        this.sel_region = [
+            { "value": 1, "label": "Region 1" },
+            { "value": 2, "label": "Region 2" },
+            { "value": 3, "label": "Region 3" }
+        ]
+
+        this.sel_hub = [
+            { "value": 1, "label": "Hub 1", "parentValue": 1 },
+            { "value": 2, "label": "Hub 2", "parentValue": 1 },
+            { "value": 3, "label": "Hub 3", "parentValue": 1 },
+            { "value": 4, "label": "Hub 4", "parentValue": 1 },
+            { "value": 5, "label": "Hub 5", "parentValue": 2 },
+            { "value": 6, "label": "Hub 6", "parentValue": 2 },
+            { "value": 7, "label": "Hub 7", "parentValue": 2 },
+            { "value": 8, "label": "Hub 8", "parentValue": 3 },
+            { "value": 9, "label": "Hub 9", "parentValue": 3 }
+        ]
+
+        this.sel_segment = [
+            { "value": 1, "label": "Segment 1", "parentValue": 1 },
+            { "value": 2, "label": "Segment 2", "parentValue": 1 },
+            { "value": 3, "label": "Segment 3", "parentValue": 1 },
+            { "value": 4, "label": "Segment 4", "parentValue": 1 },
+            { "value": 5, "label": "Segment 5", "parentValue": 2 },
+            { "value": 6, "label": "Segment 6", "parentValue": 2 },
+            { "value": 7, "label": "Segment 7", "parentValue": 2 },
+            { "value": 8, "label": "Segment 8", "parentValue": 3 },
+            { "value": 9, "label": "Segment 9", "parentValue": 3 }
+        ]
+
+        this.sel_entity = [
+            { "value": 1, "label": "Entity 1", "parentValue": 1 },
+            { "value": 2, "label": "Entity 2", "parentValue": 1 },
+            { "value": 3, "label": "Entity 3", "parentValue": 1 },
+            { "value": 4, "label": "Entity 4", "parentValue": 1 },
+            { "value": 5, "label": "Entity 5", "parentValue": 2 },
+            { "value": 6, "label": "Entity 6", "parentValue": 2 },
+            { "value": 7, "label": "Entity 7", "parentValue": 2 },
+            { "value": 8, "label": "Entity 8", "parentValue": 8 },
+            { "value": 9, "label": "Entity 9", "parentValue": 8 }
+        ]
+
+        this.sel_bp_office_address = [
+            { "value": 1, "label": "Address 1" },
+            { "value": 2, "label": "Address 2" },
+            { "value": 3, "label": "Address 3" }
+        ]
+    }
 }
--- src/views/widgets/user-panels/user-panel-mrt.ts
+++ src/views/widgets/user-panels/user-panel-mrt.ts
@@ -2,10 +2,28 @@ import { bindable } from 'aurelia-framework';
 
 export class UserPanelMrt {
     @bindable user;
+    public sel_member_status;
+    public sel_vest_colour;
     public sel_languages;
     public sel_languageLevels;
 
+
     constructor() {
+        this.sel_member_status = [
+            { "value": 1, "label": "Status 1" },
+            { "value": 2, "label": "Status 2" },
+            { "value": 3, "label": "Status 3" }
+        ]
+
+        this.sel_vest_colour = [
+            { "value": 1, "label": "Red 1" },
+            { "value": 2, "label": "Yellow 2" },
+            { "value": 3, "label": "Green 3" },
+            { "value": 4, "label": "Blue 4" },
+            { "value": 5, "label": "Pink 5" },
+            { "value": 6, "label": "Purple 6" }
+        ]
+
         this.sel_languages = [
             { "value": 1, "label": "Czech" },
             { "value": 2, "label": "German" },
