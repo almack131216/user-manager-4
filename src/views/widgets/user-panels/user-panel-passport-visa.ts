5b87e95d4e414b1e3f00e149aaacd4676ee8c7fa
Alex Mackenzie <AMackenzie@agily.local>
2017-02-01 11:59:06 +0100

    DEV - add visas html

--- src/views/widgets/user-panels/user-panel-passport-visa.ts
+++ src/views/widgets/user-panels/user-panel-passport-visa.ts
@@ -5,6 +5,8 @@ export class UserPanelPassportVisa {
     @bindable user;
     public sel_passport_types;
     public sel_passport_nationalities;
+    public sel_visa_countries;
+    public sel_visa_types;
 
     constructor() {
 
@@ -20,6 +22,20 @@ export class UserPanelPassportVisa {
             { "value": 2, "label": "British 2" },
             { "value": 3, "label": "American 3" },
             { "value": 4, "label": "Canadian 4" }
+        ]
+
+        this.sel_visa_countries = [
+            { "value": 1, "label": "Country 1" },
+            { "value": 2, "label": "Country 2" },
+            { "value": 3, "label": "Country 3" },
+            { "value": 4, "label": "Country 4" }
+        ]
+
+        this.sel_visa_types = [
+            { "value": 1, "label": "Visa Type 1" },
+            { "value": 2, "label": "Visa Type 2" },
+            { "value": 3, "label": "Visa Type 3" },
+            { "value": 4, "label": "Visa Type 4" }
         ]
     }
 
