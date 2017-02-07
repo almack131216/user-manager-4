e1d12035366774e3f2316b337530214609facc1c
Alex Mackenzie <AMackenzie@agily.local>
2017-02-07 10:57:30 +0100

    include constants

---
+++ src/resources/constants.ts
@@ -0,0 +1,12 @@
+export const debugConsoleLog = false;
+export const debugShowOutput = false;
+export const COPYRIGHT = '© 2017 BP p.l.c.';
+export const SITE_OWNER_ABBR = 'BP';
+export const SITE_OWNER = 'British Petroleum';
+export const SITE_NAME_ABBR = 'MRT';
+export const SITE_NAME = 'Mutual Response Team';
+export const AYS = "Are you sure";
+export const AYS_DELETE = "Are you sure you want to delete";
+export const BTN_XC_Expand = "Expand All";
+export const BTN_XC_Collapse = "Collapse All";
+export const MSG_TRAINING = "<p>No training is mandatory or required. Roles that require specific training will be selected based on responses below.</p>";
