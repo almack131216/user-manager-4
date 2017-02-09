f2c7e5a0b131afb5f67d89fa2d71530b20e2e211
Alex Mackenzie <alex.mackenzie@intelogy.co.uk>
2017-02-09 18:43:19 +0100

    HACK - training list + collapsable on header click

--- src/views/widgets/user-panels/user-panel-mrt-role.ts
+++ src/views/widgets/user-panels/user-panel-mrt-role.ts
@@ -4,10 +4,13 @@ import { Lookups } from '../../../resources/lookups';
 @autoinject
 export class UserPanelMrtRole {
     @bindable user;
+    lkp_coatSizes;
     lkp_primaryPositions;
     lkp_secondaryPositions;
 
     constructor(lookups:Lookups) {
+        this.lkp_coatSizes = lookups.lkp_coatSizes
+
         this.lkp_primaryPositions = lookups.lkp_primaryPositions
 
         this.lkp_secondaryPositions = lookups.lkp_secondaryPositions;
--- src/views/widgets/user-panels/user-panel-mrt.ts
+++ src/views/widgets/user-panels/user-panel-mrt.ts
@@ -5,7 +5,6 @@ import { Lookups } from '../../../resources/lookups';
 export class UserPanelMrt {
     @bindable user;
     public lkp_member_status;
-    public lkp_coatSizes;
     public lkp_languages;
     public lkp_languageLevel;
     public lkp_languages_limitTo = 5;
@@ -13,8 +12,6 @@ export class UserPanelMrt {
 
     constructor(lookups:Lookups) {
         this.lkp_member_status = lookups.lkp_member_status
-
-        this.lkp_coatSizes = lookups.lkp_coatSizes
 
         this.lkp_languages = lookups.lkp_languages
 
