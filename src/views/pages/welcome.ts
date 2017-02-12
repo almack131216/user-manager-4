82b51f9fef45c1ae771ea8ae70e843f2115647f1
alex mack <alex mack>
2017-02-12 22:55:30 +0100

    CR

--- src/views/pages/welcome.ts
+++ src/views/pages/welcome.ts
@@ -3,31 +3,8 @@ import { EventAggregator } from 'aurelia-event-aggregator';
 import * as Constants from '../../resources/constants';
 const CV = Constants
 import { Lookups } from '../../resources/lookups';
-import { ProfileState } from '../../profile-state';
-import { WebAPIUsers } from '../../api/web-api-users';
-
-//@autoinject
-@inject(WebAPIUsers,EventAggregator,ProfileState)
 
 export class Welcome {
-
-routeConfig;
-public CV = CV;
+  public CV = CV;
   title = '';
-  myState;
-  @bindable currentUser;
-
-  constructor(myState:ProfileState) {
-    this.myState = myState;
-    //this.myProfile = myState
-    //alert('welcome.ts | const | ' + JSON.stringify(myState) + ' / ');
-    // if (CV.debugConsoleLog) console.log('welcome.ts | const ' + JSON.stringify(myState) );
-  }
-
-  async created(){
-    // alert('welcome.ts | created' + JSON.stringify(this.myState) + ' / ' + this.isMember2);
-    // if (CV.debugConsoleLog) console.log('welcome.ts | created: ' + JSON.stringify(this.myState) );
-    // if(this.myState.myProfile) this.isMember2 = this.myState.myProfile.currentUser.isMember;
-  }
-
 }
--- src/views/ui/nav-bar.ts
+++ src/views/ui/nav-bar.ts
@@ -2,15 +2,8 @@ import {bindable} from 'aurelia-framework';
 import * as Constants from '../../resources/constants';
 const CV = Constants
 
-import {Router} from 'aurelia-router';
-import {Parent} from 'aurelia-framework';
-
 
 export class NavBar {
-  static inject() {
-    return [Parent.of(Router)];
-  }
-
   @bindable router = null;
   @bindable currentUser = null;
 
@@ -20,21 +13,11 @@ export class NavBar {
     this.router = router;
   }
 
-  attached(){
-    console.log('nav-bar.ts | attached: ' + this.router.navigation[0].isMemberOnly );
-  }
-
   hasAccess(getUser,getPage){
-    console.log('hasAccess: ' + JSON.stringify(getUser) + ' / ' + getPage + ' (' + getPage.settings.data.isMemberOnly + ')');
+    //console.log('hasAccess: ' + JSON.stringify(getUser) + ' / ' + getPage + ' (' + getPage.settings.data.isMemberOnly + ')');
     if(!getUser) return false;
     if(!getPage.settings.data.isMemberOnly || (getUser.isMember && getPage.settings.data.isMemberOnly==true)) return true;
     return false;
-  }
-  
-  get isLoggedIn(){
-
-    //userInfo is an object that is updated on authentication
-    return true;//this.currentUser.isMember;
   }
 
 }
