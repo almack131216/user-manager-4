0c45941e353a981cd42c08b4719f2d0cfa89c1be
alex mack <alex mack>
2017-02-13 23:22:38 +0100

    HW - header / nav styling

--- src/views/ui/nav-bar.ts
+++ src/views/ui/nav-bar.ts
@@ -1,8 +1,10 @@
-import {bindable} from 'aurelia-framework';
+import {bindable,inject,computedFrom} from 'aurelia-framework';
 import * as Constants from '../../resources/constants';
 const CV = Constants
 
+//import { Router } from 'aurelia-router';
 
+//@inject(Router)
 export class NavBar {
   @bindable router = null;
   @bindable currentUser = null;
@@ -19,5 +21,20 @@ export class NavBar {
     if(!getPage.settings.data.isMemberOnly || (getUser.isMember && getPage.settings.data.isMemberOnly==true)) return true;
     return false;
   }
+
+  attached() {
+     //  this.message = this.router.currentInstruction.config.title;
+    }
+    
+    @computedFrom('router.currentInstruction')
+    get routeName() {
+      if (this.router.currentInstruction !== null)
+        return this.router.currentInstruction.config.name;
+    }
+
+  getRoute() {
+     return this.router.currentInstruction.config.name; //name of the route
+     //return this.router.currentInstruction.config.moduleId; //moduleId of the route
+   }
 
 }
