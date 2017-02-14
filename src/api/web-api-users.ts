b449ca82318b8524aad54fc90b02452564a4e9ee
Alex Mackenzie <alex.mackenzie@intelogy.co.uk>
2017-02-14 16:59:24 +0100

    CR - enable edit btn ifMember on user list

--- src/api/web-api-users.ts
+++ src/api/web-api-users.ts
@@ -15,7 +15,7 @@ const profileUrl = '../../MRT.Api.Web/views/global';
 const views_welcome = '../../MRT.Api.Web/views/welcome';
 const data_users_all = '../../MRT.Api.Web/data/users/query';
 const data_users_X_profile = '../../MRT.Api.Web/data/users/5/profile';//'src/api/dummy-user-all.json';// 
-const views_profileform_X = '../../MRT.Api.Web/views/profileform/5';
+const views_profileform_X = '../../MRT.Api.Web/views/profileform/';
 const data_users_X = '../../MRT.Api.Web/data/users/';
 
 //  const profileUrl = 'src/api/dummy-user-all.json';
@@ -107,9 +107,8 @@ export class WebAPIUsers {
     this.isRequesting = true;
     return new Promise(resolve => {
       setTimeout(() => {
-        console.log('usersArr:' + usersArr);
         //let found = usersArr.filter(x => x.id == id);
-        let found = this.http.fetch(views_profileform_X)
+        let found = this.http.fetch(views_profileform_X + id)
           .then(found => found.json())
           .then(found => found);
 
